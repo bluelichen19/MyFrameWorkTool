8aca46da5925700ac42bc01e24b2c7304fbcb3e5
bluelichen19 <blueliche_n@163.com>
2016-09-06 17:51:52 +0800

    MutexLock

---
+++ MutexLock/BlueMutexLock.h
@@ -0,0 +1,6 @@
+#ifndef _BLUE_MUTEX_LOCK_H_
+#define _BLUE_MUTEX_LOCK_H_
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#endif
---
+++ MutexLock/test.cpp
@@ -0,0 +1,6 @@
+#include "BlueMutexLock.h"
+int main(int argc,char* argv[])
+{
+    int ret = 0;
+    return 0;
+}
